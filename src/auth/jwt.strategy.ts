7734de8b31952b10eede3a2dccf6f9a98d552aaa
jhonatan <jjl.valencia@yavirac.edu.ec>
2026-09-05 15:50:28 -0500

    Seguridad crítica + migraciones limpias + variables de entorno finalizado

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'farmasys_secret_key',
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username, rol: payload.rol };
+  async validate(payload: { sub: number; username: string; rol: string }) {
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      rol: payload.rol
+    };
   }
 }
