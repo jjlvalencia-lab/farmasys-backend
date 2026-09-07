72431852172924bdafd008042cc9fbea1466ccc1
jhonatan <jjl.valencia@yavirac.edu.ec>
2026-09-06 19:34:40 -0500

    fix: data-source con variables de entorno

--- data-source.ts
+++ data-source.ts
@@ -1,19 +1,16 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
 
-const isProduction = process.env.NODE_ENV === 'production';
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: isProduction
-    ? 'db.uturpmxoyhhpjskcaixk.supabase.co'
-    : 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: isProduction
-    ? 'X4v13r0412**'
-    : 'xavier444',
-  database: isProduction ? 'postgres' : 'farmasys_db',
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'xavier444',
+  database: process.env.DB_NAME || 'farmasys_db',
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
