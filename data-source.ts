d4b8a89b300638b93dffe7745632169f334f6f7b
jhonatan <jjl.valencia@yavirac.edu.ec>
2026-09-06 19:24:51 -0500

    feat: migraciones automaticas en deploy railway

--- data-source.ts
+++ data-source.ts
@@ -1,12 +1,19 @@
 import { DataSource } from 'typeorm';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: isProduction
+    ? 'db.uturpmxoyhhpjskcaixk.supabase.co'
+    : 'localhost',
   port: 5432,
   username: 'postgres',
-  password: 'xavier444',
-  database: 'farmasys_db',
+  password: isProduction
+    ? 'X4v13r0412**'
+    : 'xavier444',
+  database: isProduction ? 'postgres' : 'farmasys_db',
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
